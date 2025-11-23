016916c540c024b5701aaf20f8f267546e3e4f2a
Besnik <bmehmeti@hotmail.fr>
2025-11-23 23:21:59 +0100

    Add files via upload

---
+++ layout.js
@@ -0,0 +1,13 @@
+// Charge le header
+fetch("header.html")
+  .then(response => response.text())
+  .then(data => {
+      document.getElementById("header").innerHTML = data;
+  });
+
+// Charge le footer
+fetch("footer.html")
+  .then(response => response.text())
+  .then(data => {
+      document.getElementById("footer").innerHTML = data;
+  });
